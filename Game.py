dcc9d56f9c6e734f74e13580cfd8382ab996dcb6
asiradnan <asiradnan23@gmail.com>
2024-12-26 14:57:04 +0600

    Upward and downward movements

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 
 R,G,B = 1,1,1
+player1_y, player2_y = 0, 0
 def draw_points(x, y, size, flag):
     global R,G,B
     glPointSize(size)
@@ -65,109 +66,111 @@ def draw_circle(x_centre, y_centre, r, size=5):
             draw_points(-y + x_centre, -x + y_centre,size, False)
 
 def draw_player1():
+    global player1_y
     #1. LEG
     #Left leg
       # Green color for the rectangle
-    draw_line(30,40,40,40)  # Bottom edge
-    draw_line(40,40,40,70)  # Right edge
-    draw_line(30,70,40,70)  # Top edge
-    draw_line(30,70,30,40)  # Left edge
+    draw_line(30,player1_y+40,40,40+player1_y)  # Bottom edge
+    draw_line(40,player1_y+40,40,70+player1_y)  # Right edge
+    draw_line(30,player1_y+70,40,70+player1_y)  # Top edge
+    draw_line(30,player1_y+70,30,40+player1_y)  # Left edge
 
     #Right leg
-    draw_line(40, 40, 50, 40)  # Bottom edge
-    draw_line(50, 40, 50, 70)  # Right edge
-    draw_line(50, 70, 40, 70)  # Top edge
-    draw_line(40, 70, 40, 40)  # Left edge
+    draw_line(40,player1_y+ 40, 50, 40+player1_y)  # Bottom edge
+    draw_line(50,player1_y+ 40, 50, 70+player1_y)  # Right edge
+    draw_line(50,player1_y+ 70, 40, 70+player1_y)  # Top edge
+    draw_line(40,player1_y+ 70, 40, 40+player1_y)  # Left edge
 
     #2. Shoe
     #Left Shoe
-    draw_line(30, 40, 40, 40)  # Bottom edge
-    draw_line(40, 40, 40, 30)  # Right edge
-    draw_line(30, 30, 40, 30)  # Top edge
-    draw_line(30, 30, 30, 40)  # Left edge
+    draw_line(30,player1_y+ 40, 40, 40+player1_y)  # Bottom edge
+    draw_line(40,player1_y+ 40, 40, 30+player1_y)  # Right edge
+    draw_line(30,player1_y+ 30, 40, 30+player1_y)  # Top edge
+    draw_line(30,player1_y+ 30, 30, 40+player1_y)  # Left edge
 
     # Right Shoe
-    draw_line(40, 40, 50, 40)  # Bottom edge
-    draw_line(50, 40, 50, 30)  # Right edge
-    draw_line(50, 30, 40, 30)  # Top edge
-    draw_line(40, 30, 40, 40)  # Left edge
+    draw_line(40,player1_y+ 40, 50, 40+player1_y)  # Bottom edge
+    draw_line(50,player1_y+ 40, 50, 30+player1_y)  # Right edge
+    draw_line(50,player1_y+ 30, 40, 30+player1_y)  # Top edge
+    draw_line(40,player1_y+ 30, 40, 40+player1_y)  # Left edge
 
     #3. Body
-    draw_line(30, 70, 50, 70)  # Bottom edge
-    draw_line(50, 70, 50, 100)  # Right edge
-    draw_line(50, 100, 30, 100)  # Top edge
-    draw_line(30, 100, 30, 70)  # Left edge
+    draw_line(30,player1_y+ 70, 50, 70+player1_y)  # Bottom edge
+    draw_line(50,player1_y+ 70, 50, 100+player1_y)  # Right edge
+    draw_line(50,player1_y+ 100, 30, 100+player1_y)  # Top edge
+    draw_line(30,player1_y+ 100, 30, 70+player1_y)  # Left edge
 
     #4. Head
-    draw_circle(40, 110, 10)
+    draw_circle(40, 110+player1_y, 10)
 
     #5.hand
-    draw_line(50,90,60,90)  # Bottom edge
-    draw_line(60,90,60,100)  # Right edge
-    draw_line(60,100,50,100)
-    draw_line(50,100,50,90)
+    draw_line(50,player1_y+90,60,90+player1_y)  # Bottom edge
+    draw_line(60,player1_y+90,60,100+player1_y)  # Right edge
+    draw_line(60,player1_y+100,50,100+player1_y)
+    draw_line(50,player1_y+100,50,90+player1_y)
 
     #6. gun
-    draw_line(60,100,90,100)  # Bottom edge
-    draw_line(90,100,95,90)  # Right edge
-    draw_line(95,90,65,95)  # Top edge
-    draw_line(65,95,65,80)  # Left edge
-    draw_line(65,80,60,80)
-    draw_line(60,80,60,100)
-    draw_line(60,100,90,100)
+    draw_line(60,player1_y+100,90,100+player1_y)  # Bottom edge
+    draw_line(90,player1_y+100,95,90+player1_y)  # Right edge
+    draw_line(95,player1_y+90,65,95+player1_y)  # Top edge
+    draw_line(65,player1_y+95,65,80+player1_y)  # Left edge
+    draw_line(65,player1_y+80,60,80+player1_y)
+    draw_line(60,player1_y+80,60,100+player1_y)
+    draw_line(60,player1_y+100,90,100+player1_y)
 
 
 def draw_player2():
+    global player2_y
     #1. LEG
     #Left leg
-    draw_line(760,40,770,40)  # Bottom edge  (X: x1=800-40,x2=800-30)
-    draw_line(770,40,770,70)  # Right edge
-    draw_line(760,70,770,70)  # Top edge
-    draw_line(760,70,760,40)  # Left edge
+    draw_line(760,+player2_y+40,770,40+player2_y)  # Bottom edge  (X: x1=800-40,x2=800-30+player2_y)
+    draw_line(770,+player2_y+40,770,70+player2_y)  # Right edge
+    draw_line(760,+player2_y+70,770,70+player2_y)  # Top edge
+    draw_line(760,+player2_y+70,760,40+player2_y)  # Left edge
 
     #Right leg
-    draw_line(750, 40, 760, 40)  # Bottom edge  (X: x1=800-50,x2=800-40)
-    draw_line(760, 40, 760, 70)  # Right edge
-    draw_line(760, 70, 750, 70)  # Top edge
-    draw_line(750, 70, 750, 40)  # Left edge
+    draw_line(750,+player2_y+ 40, 760, 40+player2_y)  # Bottom edge  (X: x1=800-50,x2=800-40+player2_y)
+    draw_line(760,+player2_y+ 40, 760, 70+player2_y)  # Right edge
+    draw_line(760,+player2_y+ 70, 750, 70+player2_y)  # Top edge
+    draw_line(750,+player2_y+ 70, 750, 40+player2_y)  # Left edge
 
     #2. Shoe
     #Left Shoe
-    draw_line(760, 40, 770, 40)  # Bottom edge
-    draw_line(770, 40, 770, 30)  # Right edge
-    draw_line(760, 30, 770, 30)  # Top edge
-    draw_line(760, 30, 760, 40)  # Left edge
+    draw_line(760,+player2_y+ 40, 770, 40+player2_y)  # Bottom edge
+    draw_line(770,+player2_y+ 40, 770, 30+player2_y)  # Right edge
+    draw_line(760,+player2_y+ 30, 770, 30+player2_y)  # Top edge
+    draw_line(760,+player2_y+ 30, 760, 40+player2_y)  # Left edge
 
     # Right Shoe
-    draw_line(750, 40, 760, 40)  # Bottom edge
-    draw_line(760, 40, 760, 30)  # Right edge
-    draw_line(760, 30, 750, 30)  # Top edge
-    draw_line(750, 30, 750, 40)  # Left edge
+    draw_line(750,+player2_y+ 40, 760, 40+player2_y)  # Bottom edge
+    draw_line(760,+player2_y+ 40, 760, 30+player2_y)  # Right edge
+    draw_line(760,+player2_y+ 30, 750, 30+player2_y)  # Top edge
+    draw_line(750,+player2_y+ 30, 750, 40+player2_y)  # Left edge
 
     #3. Body
-    draw_line(750, 70, 770, 70)  # Bottom edge  (x1=800-50,x2=800-30)
-    draw_line(770, 70, 770, 100)  # Right edge
-    draw_line(770, 100, 750, 100)  # Top edge
-    draw_line(750, 100, 750, 70)  # Left edge
+    draw_line(750,+player2_y+ 70, 770, 70+player2_y)  # Bottom edge  (x1=800-50,x2=800-30+player2_y)
+    draw_line(770,+player2_y+ 70, 770, 100+player2_y)  # Right edge
+    draw_line(770,+player2_y+ 100, 750, 100+player2_y)  # Top edge
+    draw_line(750,+player2_y+ 100, 750, 70+player2_y)  # Left edge
 
 
     #4. Head
-    draw_circle(760, 110, 10) #x=800-40
+    draw_circle(760, 110+player2_y, 10) #x=800-40
 
     #5.hand
-    draw_line(740,90,750,90)  # Bottom edge  #x1=800-60, x2=800-50
-    draw_line(750,90,750,100)  # Right edge
-    draw_line(750,100,740,100)
-    draw_line(740,100,740,90)
+    draw_line(740,+player2_y+90,750,90+player2_y)  # Bottom edge  #x1=800-60, x2=800-50
+    draw_line(750,+player2_y+90,750,100+player2_y)  # Right edge
+    draw_line(750,+player2_y+100,740,100+player2_y)
+    draw_line(740,+player2_y+100,740,90+player2_y)
 
     #6. gun
-    draw_line(710,100,740,100)
-    draw_line(710,100,710,90)
-    draw_line(735,95,710,90)
-    draw_line(735,95,735,80)
-    draw_line(740,80,735,80)
-    draw_line(740,80,740,100)
-    draw_line(710,100,740,100)
+    draw_line(710,+player2_y+100,740,100+player2_y)
+    draw_line(710,+player2_y+100,710,90+player2_y)
+    draw_line(735,+player2_y+95,710,90+player2_y)
+    draw_line(735,+player2_y+95,735,80+player2_y)
+    draw_line(740,+player2_y+80,735,80+player2_y)
+    draw_line(740,+player2_y+80,740,100+player2_y)
+    draw_line(710,+player2_y+100,740,100+player2_y)
 
 def draw_walls():
     draw_line(120,0,120,220)
@@ -195,6 +198,25 @@ def showScreen():
 
     glutSwapBuffers()
 
+def redraw():
+    
+    glutPostRedisplay()
+
+def keyboardListener(key, x, y):
+    global player1_y
+    if key == b'w':
+        player1_y += 10
+    elif key == b's':
+        player1_y -= 10
+
+def specialKeyListener(key, x, y):
+    global  player2_y
+    if key == GLUT_KEY_UP:
+        player2_y += 10
+    elif key == GLUT_KEY_DOWN:
+        player2_y -= 10
+        
+
 
 glutInit()
 glutInitDisplayMode(GLUT_RGBA)
@@ -202,5 +224,9 @@ glutInitWindowSize(800, 600)  #window size
 glutInitWindowPosition(0, 0)
 wind = glutCreateWindow(b"Game!")  #window name
 glutDisplayFunc(showScreen)
+glutKeyboardFunc(keyboardListener)
+glutSpecialFunc(specialKeyListener)
+# glutMouseFunc(mouseListener)
+glutIdleFunc(redraw)
 make_game_over = False
 glutMainLoop()
